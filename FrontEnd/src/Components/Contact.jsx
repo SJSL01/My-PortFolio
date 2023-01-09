b2ba6f9e29f802fb9812f05f4af845dcee548532
SJSL01 <simran08lall@gmail.com>
2023-01-09 17:29:52 +0530

    fixes

--- FrontEnd/src/Components/Contact.jsx
+++ FrontEnd/src/Components/Contact.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import "../Styles/Contact.css"
 import axios from "axios"
 import toast, { Toaster } from "react-hot-toast"
 export default function Contact() {
+
+  const message = useRef()
+
   const [contactInfo, setContactInfo] = useState(
     {
       name: "",
@@ -13,6 +16,7 @@ export default function Contact() {
   const handleInput = (e) => {
     const { name, value } = e.target
     setContactInfo({ ...contactInfo, [name]: value })
+    message.current.scrollIntoView({ behavior: "smooth" })
   }
 
   const handleSubmit = async (e) => {
@@ -53,13 +57,13 @@ export default function Contact() {
           <h2 style={{ margin: "0 auto" }}>Contact ME</h2>
           <div>
             <label htmlFor="email">Email</label>
-            <input placeholder="Email" onChange={(e) => { handleInput(e) }} value={contactInfo.email} type="text" id="email" name="email" />
+            <input placeholder="Email" onChange={(e) => { handleInput(e) }} value={contactInfo.email} type="email" id="email" name="email" />
           </div>
           <div>
             <label htmlFor="name">Name</label>
             <input placeholder="Name" onChange={(e) => { handleInput(e) }} value={contactInfo.name} type="text" id="name" name="name" />
           </div>
-          <div>
+          <div ref={message}>
             <label htmlFor="name">Message</label>
             <textarea onChange={(e) => { handleInput(e) }}
               value={contactInfo.message} type="text" name="message" />
